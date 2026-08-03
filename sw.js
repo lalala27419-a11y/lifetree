4af122f2f3894465be5d4aaaddbaeef189cf247e
lalala27419-a11y <lalala27419@gmail.com>
2026-08-03 10:12:04 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+/* 离线缓存：装到主屏后断网也能用 */
+const CACHE = 'lifetree-v7';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) =>
+    Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+/* 网络优先：有网时总是拿最新版本，断网才回退到缓存
+   这样更新程序后，手机上打开就是新的，不会卡在旧版本 */
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(e.request).then((hit) => hit || caches.match('./index.html')))
+  );
+});
